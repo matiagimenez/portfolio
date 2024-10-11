ef10801cbf1fcc673765329cb722178ca59d67d8
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-10-11 10:03:44 -0300

    fix(config): add shadcn/ui config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
 	return (
 		<main className='min-h-full flex justify-center items-center'>
 			<h1 className='text-purple-500 font-bold text-3xl'>Hello world!</h1>
+			<Button>Click me</Button>
 		</main>
 	);
 }
