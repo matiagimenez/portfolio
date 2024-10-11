399a7c19338a41d2fd8e8480bcacbf9436e52975
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-10-11 12:01:15 -0300

    fix(config): update default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,7 @@ export default function RootLayout({
 			<body className={`h-full ${openSans.className} px-12`}>
 				<ThemeProvider
 					attribute='class'
-					defaultTheme='system'
-					enableSystem
+					defaultTheme='light'
 					disableTransitionOnChange
 				>
 					<Header />
