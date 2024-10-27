a8d973afad953fa0a6f8de14482d9069ed6aa7f5
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-10-27 11:49:46 -0300

    fix(config): add resources page introduction section

---
+++ src/app/resources/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function Page() {
+	return (
+		<>
+			<section className='grow max-w-5xl pt-2'>
+				<h2 className='text-2xl font-bold drop-shadow-lg p-2 md:pr-3'>
+					<span className='text-pink-500 font-semibold'>
+						Resources
+					</span>
+				</h2>
+				<p className='text-lg pb-2 px-2 md:pr-3'>
+					I have gathered a variety of resources that I’ve found
+					helpful or inspiring in my development journey.
+				</p>
+				<p className='text-lg pb-2 px-2 md:pr-3'>
+					These include in-depth articles, videos, and code
+					repositories that have offered guidance, sparked creativity,
+					and enriched my
+					<span className='text-pink-500 font-semibold inline-block mx-1'>
+						technical knowledge
+					</span>
+					in software development.
+				</p>
+				<p className='text-lg pb-2 px-2 md:pr-3'>
+					I hope these resources provide you with valuable insights
+					and inspire new ideas, just as they have for me.
+				</p>
+			</section>
+		</>
+	);
+}
