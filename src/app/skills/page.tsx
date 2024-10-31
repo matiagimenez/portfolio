3bb351b0ad054b8b406b0ed3d0ef1670b15d65a5
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-10-30 21:26:43 -0300

    fix(config): add skills data fetching

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import axios from 'axios';
+import { Skill } from '@/types/skill';
 
-export default function Page() {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export default async function Page() {
+	const response = await axios.get(`${BASE_URL}/api/skills`);
+	const skills: Skill[] = response.data;
+
 	return (
 		<>
 			<section className='grow max-w-5xl pt-2'>
 				<h2 className='text-2xl font-bold drop-shadow-lg p-2 md:pr-3'>
-					<span className='text-pink-500 font-semibold'>Tech Stack</span>
+					<span className='text-pink-500 font-semibold'>
+						Tech Stack
+					</span>
 				</h2>
 				<p className='text-lg pb-2 px-2 md:pr-3'>
 					Here, I want to share the skills I’ve developed in my
@@ -18,6 +27,7 @@ export default function Page() {
 					</span>
 					to stay current in the fast-evolving world of technology.
 				</p>
+				<p>{skills.map((skill) => JSON.stringify(skill))}</p>
 			</section>
 		</>
 	);
