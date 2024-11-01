d5137a922778b3ac214f9d5788e8738847561e61
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-11-01 18:42:03 -0300

    fix(config): remove animation

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 import { Categories } from '@/types/skill';
-import * as motion from 'framer-motion/client';
 import { ParticlesEffect, SkillSection } from '@/components/ui';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -13,37 +12,26 @@ export default async function Component() {
 	return (
 		<section className='grow max-w-5xl pt-4'>
 			<ParticlesEffect />
-			<motion.section
-				initial={{ opacity: 0, y: 20 }}
-				animate={{ opacity: 1, y: 0 }}
-				transition={{ duration: 0.5 }}
-				className='mb-6'
-			>
-				<h2 className='text-3xl font-bold mb-4'>
+			<section className='mb-6'>
+				<h2 className='text-2xl md:text-3xl font-bold mb-4'>
 					My <span className='text-pink-500'>Tech Stack</span>
 				</h2>
-				<p className='text-lg max-w-2xl mb-4'>
+				<p className='text-lg mb-4'>
 					Here, I want to share the skills I have developed in my
 					journey as a software developer.
 				</p>
-				<p className='text-lg max-w-2xl'>
+				<p className='text-lg'>
 					With a strong commitment to learning and growth, I am
 					<span className='text-pink-500 font-semibold mx-1'>
 						constantly building on my expertise
 					</span>
 					to stay current in the fast-evolving world of technology.
 				</p>
-			</motion.section>
+			</section>
 
-			<motion.section
-				initial={{ opacity: 0, y: 20 }}
-				animate={{ opacity: 1, y: 0 }}
-				transition={{ duration: 0.5, delay: 0.2 }}
-			>
-				<SkillSection title='Frontend' skills={frontend} />
-				<SkillSection title='Backend' skills={backend} />
-				<SkillSection title='Tools' skills={tools} />
-			</motion.section>
+			<SkillSection title='Frontend' skills={frontend} />
+			<SkillSection title='Backend' skills={backend} />
+			<SkillSection title='Tools' skills={tools} />
 		</section>
 	);
 }
