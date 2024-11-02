d6b450e65a0849f7a5d1d42b6f062656a4f9b25d
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-11-01 21:40:46 -0300

    fix(config): add z-index to skills section

--- src/components/ui/skills/skills-section.tsx
+++ src/components/ui/skills/skills-section.tsx
@@ -7,11 +7,11 @@ interface SkillSectionProps {
 }
 
 export const SkillSection = ({ title, skills }: SkillSectionProps) => (
-	<Card className='my-8 mt-0 bg-background border-2 border-pink-500 rounded-lg overflow-hidden shadow-lg'>
+	<Card className='my-8 mt-0 bg-background border-2 border-pink-500 rounded-lg overflow-hidden shadow-lg z-10 relative'>
 		<CardHeader className='bg-pink-500 text-white'>
 			<h3 className='text-xl font-bold'>{title}</h3>
 		</CardHeader>
-		<CardContent className='p-6'>
+		<CardContent className='p-6 bg-background z-10'>
 			<div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4'>
 				{skills.map((skill) => (
 					<SkillCard key={skill.name} {...skill} />
