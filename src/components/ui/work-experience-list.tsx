7e7b9ca57a84bee9de141acf7f9aac649f80663c
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-11-24 13:33:32 -0300

    chore(config): reorganize components

--- src/components/ui/work-experience-list.tsx
+++ src/components/ui/work-experience-list.tsx
@@ -20,7 +20,7 @@ export const WorkExperienceList = async () => {
   const experiences: WorkExperience[] = response.data;
 
   return (
-    <section className='mt-10 px-2 max-w-6xl mb-6 bg-background w-full z-50'>
+    <section className='mt-10 px-2 max-w-6xl mb-6 bg-background w-full z-40'>
       <h2 className='flex items-center gap-2 text-2xl font-bold text-pink-500'>
         Work experience
       </h2>
